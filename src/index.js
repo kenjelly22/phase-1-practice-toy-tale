f8af8277ffbdbe10cbdec88571408a985b41044f
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-28 15:45:30 -0400

    Fix card innerHTML so cards align side by side

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIBLES
 const toyContainer = document.querySelector("#toy-collection")
 const likeButton = document.querySelector(".like-btn")
 
@@ -16,21 +17,37 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
+  // FETCH REQUEST
+
   fetch("http://localhost:3000/toys")
     .then((res) => res.json())
-    .then((data) => data.forEach((toy) => renderCollection(toy)))
-  debugger
+    .then((toys) => {
+      console.log(toys)
+      toys.forEach((toy) => renderCollection(toy))
+    })
 })
 
+// RENDER FUNCTIONS
 const renderCollection = (toy) => {
-  let card = document.createElement("div")
+  let card = document.createElement("card")
 
   card.innerHTML = `
     <div class="card">
-      <h2>Woody</h2>
-      <img src="[toy_image_url] class="toy-avatar" />
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
       <p>4 Likes</p>
-      <button class="like-btn" id="[toy_id]">Like ❤️</button>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>
     </div>`
   toyContainer.appendChild(card)
 }
+
+// Toy Data =
+// Object { id: "1", name: "Woody", image: "http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png", likes: 5 }
+// ​
+// id: "1"
+// ​
+// image: "http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png"
+// ​
+// likes: 5
+// ​
+// name: "Woody"
