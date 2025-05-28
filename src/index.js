f7660141a128ae26b3ba8db976193f3f41540774
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-28 14:36:52 -0400

    Make GET request to fetch all the toy objects. Make a card for each toy and add to toy-collection div

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-let addToy = false;
+const toyContainer = document.querySelector("#toy-collection")
+const likeButton = document.querySelector(".like-btn")
+
+let addToy = false
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  const addBtn = document.querySelector("#new-toy-btn")
+  const toyFormContainer = document.querySelector(".container")
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
-    addToy = !addToy;
+    addToy = !addToy
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = "block"
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainer.style.display = "none"
     }
-  });
-});
+  })
+
+  fetch("http://localhost:3000/toys")
+    .then((res) => res.json())
+    .then((data) => data.forEach((toy) => renderCollection(toy)))
+  debugger
+})
+
+const renderCollection = (toy) => {
+  let card = document.createElement("div")
+
+  card.innerHTML = `
+    <div class="card">
+      <h2>Woody</h2>
+      <img src="[toy_image_url] class="toy-avatar" />
+      <p>4 Likes</p>
+      <button class="like-btn" id="[toy_id]">Like ❤️</button>
+    </div>`
+  toyContainer.appendChild(card)
+}
