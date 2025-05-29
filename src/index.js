e663de635dac1ca214266fa6d547bb2654284091
Jen Kelly <jen.kelly@powerhrg.com>
2025-05-28 22:44:07 -0400

    Get current like count from clicked card and increment like count

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // GLOBAL VARIBLES
 const toyContainer = document.querySelector("#toy-collection")
-const likeButton = document.querySelector(".like-btn")
 const toysUrl = "http://localhost:3000/toys"
+const inputForm = document.querySelector("form")
+const [nameInput, imageInput] = document.querySelectorAll("input")
 
 let addToy = false
 
@@ -18,15 +19,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
-  // HTTP REQUESTS
-
+  // GET REQUEST
   fetch(toysUrl)
     .then((res) => res.json())
     .then((toys) => {
       toys.forEach((toy) => renderCollection(toy))
     })
 
-  const createNewToy = () => {
+  // POST REQUEST
+  const postNewToy = (name, image, likes) => {
     fetch(toysUrl, {
       method: "POST",
       headers: {
@@ -34,14 +35,25 @@ document.addEventListener("DOMContentLoaded", () => {
         Accept: "application/json",
       },
       body: JSON.stringify({
-        type: "text",
-        name: "name",
-        value: "",
-        placeholder: "Enter a toy's name...",
-        class: "input-text",
+        name: name,
+        image: image,
+        likes: likes,
       }),
     })
+      .then((res) => res.json())
+      .then((newToy) => {
+        renderCollection(newToy)
+      })
   }
+
+  // INPUT FORM
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const name = nameInput.value
+    const image = imageInput.value
+    const likes = 0
+    postNewToy(name, image, likes)
+  })
 
   // RENDER FUNCTIONS
   const renderCollection = (toy) => {
@@ -50,9 +62,31 @@ document.addEventListener("DOMContentLoaded", () => {
     <div class="card">
       <h2>${toy.name}</h2>
       <img src="${toy.image}" class="toy-avatar" />
-      <p>4 Likes</p>
+      <p>${toy.likes} Likes</p>
       <button class="like-btn" id="${toy.id}">Like ❤️</button>
     </div>`
     toyContainer.appendChild(card)
   }
+  // LIKE BUTTON
+
+  toyContainer.addEventListener("click", (event) => {
+    if ((event.target.classList = "like-btn")) {
+      const toyID = event.target.id
+      const card = document.querySelector(".card")
+      const pLikes = card.querySelector("p")
+      let currentLikes = parseInt(pLikes.innerHTML)
+      currentLikes++
+      pLikes.innerHTML = `${currentLikes} Likes`
+    }
+  })
 })
+
+// When I click the like button
+// I should grab the card ID to identify the card
+// and the current # of Likes
+// the like count should increment by 1 onclick
+// the text on card should update the current # of likes
+// and print: # Likes
+
+// Onclick should send Patch request
+// Will update the like count for that card ID
